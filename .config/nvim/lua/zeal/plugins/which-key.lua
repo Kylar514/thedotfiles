20f571cd825d74d3d38ae9c447e384fbd0959dd9
zeal <kylar514@gmail.com>
2024-08-10 13:27:24 -0400

    update: which-key is no longer deprecated.

--- .config/nvim/lua/zeal/plugins/which-key.lua
+++ .config/nvim/lua/zeal/plugins/which-key.lua
@@ -5,26 +5,21 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 500
     local wk = require("which-key")
-    wk.register({
-      ["<leader>"] = {
-        name = "Leader/space",
-        b = { name = "Buffer commands" },
-        c = { name = "Code actions" },
-        e = { name = "File tree" },
-        f = { name = "Telescope" },
-        l = { name = "Lazygit" },
-        m = { name = "Formater" },
-        n = { name = "Highlighting" },
-        r = { name = "LSP options" },
-        s = { name = "Window management" },
-        t = { name = "Tab management" },
-        w = { name = "Session management" },
-        x = { name = "Linting options" },
-      },
+    wk.add({
+      { "<leader>", group = "Leader/space" },
+      { "<leader>b", group = "Buffer commands" },
+      { "<leader>c", group = "Code actions" },
+      { "<leader>e", group = "File tree" },
+      { "<leader>f", group = "Telescope" },
+      { "<leader>l", group = "Lazygit" },
+      { "<leader>m", group = "Formater" },
+      { "<leader>n", group = "Highlighting" },
+      { "<leader>r", group = "LSP options" },
+      { "<leader>s", group = "Window management" },
+      { "<leader>t", group = "Tab management" },
+      { "<leader>w", group = "Session management" },
+      { "<leader>x", group = "Linting options" },
     })
-    -- local mappings = {
-    --   f = { name = "Telescope" },
-    -- }
   end,
   opts = {},
 }

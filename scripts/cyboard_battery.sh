6aa83f9c1bdf7af7f93f68807ba46f8ee6a24c89
zeal <kylar514@gmail.com>
2024-08-19 20:11:50 -0400

    feat: added keyboard bt script and hotkey

--- scripts/cyboard_battery.sh
+++ scripts/cyboard_battery.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-device_address="E1:5E:CE:2F:7A:4F"
-while true; do
-    battery_percentage=$(bluetoothctl info "$device_address" | grep "Battery Percentage" | awk '{print $3}')
-    battery_decimal=$(( 16#$battery_percentage ))
-    if [ "$battery_decimal" -eq 30 ] || [ "$battery_decimal" -eq 10 ]; then
-        notify-send "Keyboard Battery Status" "Warning: Battery Percentage is at $battery_decimal%"
+
+output=$(bluetoothctl info)
+battery_line=$(echo "$output" | grep "Battery Percentage" | tail -n 1)
+battery_percentage=$(echo "$battery_line" | grep -oP '\(\K[^)]*')
+if [ -n "$battery_percentage" ]; then
+    battery_value=$(echo "$battery_percentage" | tr -d '%')
+    if [ "$battery_value" -ge 75 ]; then
+        icon="battery-full"
+    elif [ "$battery_value" -ge 50 ]; then
+        icon="battery-good"
+    elif [ "$battery_value" -ge 25 ]; then
+        icon="battery-medium"
+    else
+        icon="battery-low"
     fi
-    sleep 600
-done
+    notify-send -i "$icon" -t 5000 "Battery Percentage: $battery_percentage"
+else
+    notify-send -i "battery-missing" "Battery Percentage not found"
+fi

cee281792a652365fe253a114d66708d1f4c1a1d
zeal <kylar514@gmail.com>
2024-11-26 21:57:10 -0500

    feat: added todo list checkboxes md.
    markdown now has the ability to use various checkboxes to declare status
    of a a item on todo list, done, in progress, high prio, low prio,
    delayed, and cancelled, marked by  , x, ~, <, >, !, - symboles and space
    in brackets.

--- .config/nvim/lua/zeal/plugins/obsidian-nvim.lua
+++ .config/nvim/lua/zeal/plugins/obsidian-nvim.lua
@@ -11,6 +11,15 @@ return {
   opts = {
     ui = {
       enable = false,
+      checkboxes = {
+        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+        ["x"] = { char = "", hl_group = "ObsidianDone" },
+        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        ["!"] = { char = "", hl_group = "ObsidianImportant" },
+        ["<"] = { char = "", hl_group = "ObsidianImportant" },
+        ["-"] = { char = "", hl_group = "ObsidianImportant" },
+      },
     },
     workspaces = {
       {
--- .config/nvim/lua/zeal/plugins/render-markdown.lua
+++ .config/nvim/lua/zeal/plugins/render-markdown.lua
@@ -8,5 +8,46 @@ return {
   },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    checkbox = {
+      enabled = true,
+      position = "inline",
+      checked = {
+        icon = "󰱒 󰚜 Done",
+        highlight = "RenderMarkdownChecked",
+      },
+      custom = {
+        todo = {
+          raw = "[~]",
+          rendered = "󰄱 󰥔 In Progress",
+          highlight = "RenderMarkdownTodo",
+          scope_highlight = nil,
+        },
+        delay = {
+          raw = "[!]",
+          rendered = "󰄱 󰲼 Delayed",
+          highlight = "DiagnosticWarn",
+          scope_highlight = nil,
+        },
+        lowprio = {
+          raw = "[>]",
+          rendered = "󰄱  Low Priority",
+          highlight = "DiagnosticOk",
+          scope_highlight = nil,
+        },
+        highprio = {
+          raw = "[<]",
+          rendered = "󰄱  High Priority",
+          highlight = "DiagnosticError",
+          scope_highlight = nil,
+        },
+        cancelled = {
+          raw = "[-]",
+          rendered = "󰰰 󰜺 Cancelled",
+          highlight = "DiffRemoved",
+          scope_highlight = nil,
+        },
+      },
+    },
+  },
 }

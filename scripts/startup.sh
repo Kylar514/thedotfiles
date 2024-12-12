e5ed73072808f9b4d9a272c7f5ae4fad4e18b916
zeal <kylar514@gmail.com>
2024-12-12 14:24:11 -0500

    feat:  default workflow apps launch on boot
    startup.sh script added.
    - brave, kitty w/ tmux, brave pwa googles messages, and kitty w/ cava
    start and move to position.
    waybar css cleaned up, removed excess comments.

---
+++ scripts/startup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+focus_window() {
+    local class="$1"
+    hyprctl dispatch focuswindow "$class"
+}
+
+workspace(){
+    local workspace="$1"
+    hyprctl dispatch movetoworkspace "$workspace"
+}
+
+move() {
+    local direction="$1"
+    hyprctl dispatch movewindow "$1"
+}
+
+resize() {
+    local width="$1"
+    local height="$2"
+    local window="$3"
+    hyprctl dispatch resizewindowpixel exact "$width" "$height","$window"
+}
+
+kitty &
+sleep 1
+focus_window "kitty"
+workspace 6
+kitty --class kitty-cava -e cava &
+sleep 1
+workspace 1
+brave &
+sleep 1
+brave --app=https://messages.google.com/web/ &
+sleep 1
+focus_window "brave-messages"
+move l
+focus_window "cava" &
+
+resize 1051 233 "kitty-cava"

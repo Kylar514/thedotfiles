4cb54e54055dbb2abdf4da0d46d04183beac9a1f
zeal <kylar514@gmail.com>
2024-12-11 21:08:04 -0500

    feat: custom waybar capslock module
    maps_lock_notify.sh launches with hyprland, every press of caps lock
    notifies user, and updates a tmp file, which the contents are than
    displayed in waybar.
    
    note: this module is only necessary because ZMK keyboards are not
    compatible with waybars built in caps lock module.

--- scripts/caps_lock_notify.sh
+++ scripts/caps_lock_notify.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
 caps_lock_state=false
+state_file=/tmp/caps_lock_state.txt
 
 evtest /dev/input/by-id/usb-ZMK_Project_Cyboard_64473BEE7CA8FBD7-event-kbd | while read -r line; do
     if echo "$line" | grep -q "EV_KEY.*KEY_CAPSLOCK"; then
         if echo "$line" | grep -q "value 1"; then
             if $caps_lock_state; then
                 caps_lock_state=false
+                echo "" > "$state_file"
                 notify-send "Caps Lock OFF"
             else
                 caps_lock_state=true
+                echo "󰪛" > "$state_file"
                 notify-send "Caps Lock ON"
             fi
         fi
     fi
 done
-#
-# caps_lock_state=false
-#
-# evtest /dev/input/by-id/usb-ZMK_Project_Cyboard_64473BEE7CA8FBD7-event-kbd | while read -r line; do
-#     if echo "$line" | grep -q "EV_KEY.*KEY_CAPSLOCK"; then
-#         if echo "$line" | grep -q "value 1"; then
-#             if $caps_lock_state; then
-#                 caps_lock_state=false
-#                 echo notify-send "Caps lock off"
-#             else
-#                 caps_lock_state=true
-#                 echo notify-send "Caps lock on"
-#             fi
-#         fi
-#     fi
-# done
